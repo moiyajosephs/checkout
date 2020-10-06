1751a60b7729bf6266758b13509ba93e03e68412
moiyajosephs <josephsmoiya@gmail.com>
2020-10-06 12:27:54 -0400

    hide shipping field

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -1,1 +1,13 @@
 // resources/javascript/index.js
+function myFunction(){
+    var ship =document.getElementById("ship-addr");
+    if (ship.checked){
+        var hide = document.getElementById('ship');
+        hide.style.display = "none";
+    }
+    else{
+        var show = document.getElementById('ship');
+        show.style.display = "block";
+    }
+    
+}
