32d60f11ceef76dcb13bb076863ab510b4b3daa3
moiyajosephs <josephsmoiya@gmail.com>
2020-10-06 14:42:52 -0400

    included ship valid

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -1,18 +1,37 @@
 // resources/javascript/index.js
 function myFunction(){
-    var ship =document.getElementById("ship-addr");
+    var ship = document.getElementById("shipaddr");
     if (ship.checked){
         var hide = document.getElementById('ship');
         hide.style.display = "none";
+        /*
+        var shipvalidate = document.querySelector('#shipaddr');
+        var countryvalidate = document.querySelector('#shipcountry');
+        var statevalidate= document.querySelector('#shipstate');
+        var zipvalidate= document.querySelector('#shipzip');
+
+        countryvalidate.setAttribute('required','required');
+        shipvalidate.setAttribute('required','required');
+        statevalidate.setAttribute('required','required');
+        zipvalidate.setAttribute('required','required');
+        */
     }
     else{ //when not clicked make required
         var show = document.getElementById('ship');
         show.style.display = "block";
-        var shipvalidate = document.getElementById('ship-address').value;
-        if (shipvalidate==""){
-            
-        }
+
         
+        var shipvalidate = document.querySelector('#shippaddr');
+        var countryvalidate = document.querySelector('#shipcountry');
+        var statevalidate= document.querySelector('#shipstate');
+        var zipvalidate= document.querySelector('#shipzip');
+
+        countryvalidate.setAttribute('required','required');
+        shipvalidate.setAttribute('required','required');
+        statevalidate.setAttribute('required','required');
+        zipvalidate.setAttribute('required','required');
+        
+      
     }
     
 }
